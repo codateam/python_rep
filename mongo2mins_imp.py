6f22b7b70b402482e19ef7710b40626bdf11dd37
umarfarouk98 <fyunusa1998@gmail.com>
2022-04-30 15:43:54 +0100

    modified Sat-Apr-2022

--- mongo2mins_imp.py
+++ mongo2mins_imp.py
@@ -3,13 +3,14 @@ import pandas as pd
 from pymongo import MongoClient
 import json
 import re
+import credentials as cstring
 
 # sudo mv /home/umarv24/Downloads/export_convert.php /var/www/html/export_convert.php
 # sudo gedit /var/www/html/export2.csvjson
 # chmod 777 /var/www/html/export_convert.php
 
 # Provide the mongodb atlas url to connect python to mongodb using pymongo
-CONNECTION_STRING = ""
+CONNECTION_STRING = cstring.CONNECTION_STRING
 # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
 client = MongoClient(CONNECTION_STRING)
 #-----collections access----------------#
@@ -58,47 +59,78 @@ def pick_allColl_data(collname):
     return allColldata
 
 
+#-----------------------------------------#
+    #------------------pick all keyword data once-----------------------#
+def pick_keyColl_data(collname):
+    allKeydata = []
+    Keyword_coll = db[collname]
+    cursor = Keyword_coll.find({})
+    for document in cursor:
+        del document['_id']
+        allKeydata.append(document)    # print(allKeydata)
+    return allKeydata
+
+
+#-----------------------------------------#
+    #------------------pick all keyword data once-----------------------#
+def multi_data_splitter(dataSource, dataIndex, keycheck, dbDatas, total_data_ids):
+    data_id = ""
+    #----------splitting the category string-----------#
+    splitted = dataSource[dataIndex][keycheck].split(",")
+    for x in range(0, len(splitted)):
+        #-------clean the whitespace frm splited names ----------#
+        data_name_split = splitted[x].strip()
+        #--------locate each category id & data from collection-------#
+        findData = dbDatas
+        #---------loop through the object cox it's mongo object-------#
+        for value in findData:
+            if value['name'] == data_name_split:
+                data_id_val = value['id']
+                data_id += str(data_id_val)+ ','
+                total_data_ids.append(data_id_val)
+            else:
+                pass
+        
+    data_id = data_id[0:len(data_id)-1]  
+    return total_data_ids
+
+
 AllCateg = pick_allColl_data("tbl_category_try")
-payload = Optpicker('export2.json')
+AllKeys = pick_keyColl_data("all_keywords")
+# payload = Optpicker('export2.json')
 # print(AllCateg)
 
-def sendToFile(maxlength):
-
+def sendToFile(filename, maxlength):
+    payload = filename
     with open("sample_export.json", "w") as outfile:
         overallArr = []
         for i in range(0,maxlength):
 
-            total_categ_ids = []
             #---------find equiv art id using artist name------#
+            total_categ_ids = []
             if payload[i]['mp3_artist'] == "":
                 categ_id = 0
-                # print(f'empty categ id: {categ_id}')
                 
             else:
                 try:
-                    categ_id = ""
-                    #----------splitting the category string-----------#
-                    splitted = payload[i]['mp3_artist'].split(",")
-                    for x in range(0, len(splitted)):
-                        #-------clean the whitespace frm splited names ----------#
-                        categ_name_split = splitted[x].strip()
-                        #--------locate each category id & data from collection-------#
-                        findcateg = AllCateg
-                        #---------loop through the object cox it's mongo object-------#
-                        for value in findcateg:
-                            if value['name'] == categ_name_split:
-                                categ_id_val = value['id']
-                                categ_id += str(categ_id_val)+ ','
-                                total_categ_ids.append(categ_id_val)
-                            else:
-                                pass
-                        
-                    categ_id = categ_id[0:len(categ_id)-1]  
-                    # print(total_categ_ids)
-
+                    key_content = 'mp3_artist'
+                    total_categ_ids = multi_data_splitter(payload, i, key_content, AllCateg, total_categ_ids)
                 except Exception as e:
                     print(e)
                 
+            
+            #---------find equiv cat id using categ name------#
+            total_keyword_ids  = []
+            if payload[i]['Keywords'] == "":
+                key_id = 0
+            
+            else:
+                try:
+                    key_content = 'mp3_artist'
+                    total_keyword_ids = multi_data_splitter(payload, i, key_content, AllKeys, total_keyword_ids)
+                except Exception as e:
+                    print(e)
+            
                     
             
             #-----datas coming from the json file-----------#
@@ -138,10 +170,10 @@ def sendToFile(maxlength):
             else:
                 keyword = payload[i]['Keywords']
 
-            if payload[i]['key_id'] == '':
-                key_id = 0
-            else:
-                key_id = int(payload[i]['key_id'])
+            # if payload[i]['key_id'] == '':
+            #     key_id = 0
+            # else:
+            #     key_id = int(payload[i]['key_id'])
 
         
             #----------regular expre to clean description nd get size---------#
@@ -154,13 +186,16 @@ def sendToFile(maxlength):
                 mp3_size = ""
 
             #---------dic data for all records to be posted on mongo-------#
+            raw_data = f"{0},{1},{2}".format(ids,album_id,mp3_duration)
+            hashed_data = data_hasher(raw_data)
             emp_rec1 = {
                 "id":ids,
+                "hashed":hashed_data,
                 "status":status,
                 "album_id":album_id,
                 "rp_id": rp_id,
                 "lang_id":lang_id,
-                "key_id":key_id,
+                "key_id":total_keyword_ids,
                 "cat_id": total_categ_ids,
                 "mp3_title": mp3_title,
                 "mp3_url": mp3_url,
@@ -180,14 +215,16 @@ def sendToFile(maxlength):
                 "cat_id": total_categ_ids,
                 "album_id":album_id,
             }
+
             overallArr.append(emp_rec1)
+            
 
     #-----------print inserting doc----------#
             # print(f"inserting {ids} records with desc {mp3_description}Title: {mp3_title}\nMp3 Url: {mp3_url}")
             # print(f"inserted record:---------------{i} out of {no_of_rec}-----------------------------------------------------------------------------------\n")
 
         json_object = json.dumps(overallArr, indent = 4)
-        outfile.write(json_object)
+        outfile.write(json_object)  
 
 
 
@@ -200,12 +237,40 @@ def readFrmFile(filename, collname):
     
     # returns JSON object as a dictionary
     data = json.load(f)
-    collname.insert_many(data)
+    print(data)
+    # collname.insert_many(data)
     
     # Closing file
     f.close()
     print("population complete! file closed")
 
-sendToFile(60000)
-readFrmFile('sample_export.json', db['tbl_mp3_try'])
+#--------------------Dispatcher------------------------#
+def take_choice(filename,collname,maxlimit):
+
+    choice = int(input("pick the operation to perform\n 1.) Read->Process->Import \n 2.) Read->Process \n "))
+    # filename = input("Input the filename e.g mydata.json: ")
+    payload = Optpicker(filename)
+
+    if choice == 1:
+        sendToFile(payload,maxlimit)
+    elif choice == 2:
+        sendToFile(payload,maxlimit)
+        readFrmFile("sample_export.json", db[collname])
+
+
+#-----------------------------------------#
+    #------------------pick all keyword data once-----------------------#
+def data_hasher(raw_data):
+    import hashlib
+    hash_obj = hashlib.md5(raw_data.encode())
+
+    return hash_obj.hexdigest()
+
+if __name__ == '__main__':
+    
+    filename = input("Input the filename e.g mydata.json: ")
+    collname = input("Input the collection name to import all lectures to: ")
+    maxlimit = int(input("Input the maximum amount of data to work on: "))
+
+    take_choice(filename, collname, maxlimit)
 # 20244,20245,20246,20247,20248,20249,20250,20251
